4cd213c95b65328939ac95c7d46f5272ca707abd
singh98442 <surendra.singh98442@gmail.com>
2022-06-23 16:59:20 +0545

    edit

--- cvproject/settings.py
+++ cvproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,6 +129,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+django_heroku.settings(locals())
 
 
